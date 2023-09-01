01eff3298f785fcfbcac30713443e103a477b73b
Alex <103459735+torn80beta@users.noreply.github.com>
2023-09-01 16:23:26 +0300

    shared

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <main>
-      <h1>Threads</h1>
-      <div>
-        <UserButton afterSignOutUrl="/" />
-      </div>
-    </main>
+    <>
+      <h1 className="head-text text-let">Home</h1>
+    </>
   );
 }
