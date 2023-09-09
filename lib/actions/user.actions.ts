ba56e680b3d5b20a22fcfd2de51ae937f0e3f88a
Alex <103459735+torn80beta@users.noreply.github.com>
2023-09-09 14:32:06 +0300

    thread tabs

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
 
 interface Params {
   userId: string;
@@ -53,5 +54,33 @@ export async function fetchUser(userId: string) {
     // .populate({ path: "communities", model: Community });
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    //Find all threads authored by user with given userId
+    // TODO: Populate community
+    const threads = await User.findOne({ _id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+
+    // console.log(threads);
+
+    return threads;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user posts: ${error.message}`);
   }
 }
